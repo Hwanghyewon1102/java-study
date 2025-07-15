ddd106d58ac5f32ea4f873abd02df7af900b6ea7
Hwanghyewon <asdd1103@naver.com>
2025-07-15 10:14:16 +0900

    초 프로젝트 설정

---
+++ chap01/src/sec03/exam02/RunStatementExample.java
@@ -0,0 +1,14 @@
+package sec03.exam02;
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1; // 변수 X를 선언하고 1을 저장
+		int y = 2; // 변수 Y를 선언하고 2를 저장
+		int result = x + y; // 변수 result를 선언하고 X와 Y를 더한 값을 저장
+		System.out.println(result); // println 메소드 호출 => 콘솔에 result값 출력
+
+	}
+
+}
