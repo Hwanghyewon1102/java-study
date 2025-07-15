6d6ed5a68a910268eebd4badea483c7f7e424fa8
'Hwanghyewon' <'asdd1103@naver.com'>
2025-07-15 17:37:43 +0900

    2025 - 07 - 15 수업
    수업 내용

---
+++ chap02/src/sec01/exam03/VariableExchangeExample.java
@@ -0,0 +1,32 @@
+package sec01.exam03;
+
+import java.util.function.ToDoubleBiFunction;
+
+public class VariableExchangeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 3;
+		int y = 5;
+		
+		System.out.println("x="+ x + " y="+ y );
+		
+		
+//		y = x ; // X에 저장된 값을 변수 Y에 저장 (복사)
+//		
+//		System.out.println("x="+ x + " y="+ y );
+
+		//x와 y의 값을 서로 교환. 즉, 바꾸고 싶다면
+		
+		int temp = x;
+		x = y;
+		y = temp;
+		
+		System.out.println("x="+ x + " y="+ y );
+		
+	
+		
+	}
+
+}
---
+++ chap02/src/sec02/exam05/StringExample.java
@@ -0,0 +1,16 @@
+package sec02.exam05;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// String : 참조타입, 타입이 대문자로 시작함에 주의!
+		String name = "황혜원";
+		String job = " ";
+		
+		System.out.println(name);
+		System.out.println(job);
+
+	}
+
+}
