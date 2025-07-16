c0310dd9ef8e77ff7386322b4a180bb5858519c5
'Hwanghyewon' <'asdd1103@naver.com'>
2025-07-16 16:28:12 +0900

    2025 / 07 / 16 오후 수업내용
    2장

---
+++ chap02/src/sec04/exam04/QStopExample.java
@@ -0,0 +1,24 @@
+package sec04.exam04;
+
+import java.io.IOException;
+
+public class QStopExample {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		int keyCode;
+		
+		while (true) { //무한 반복
+			keyCode = System.in.read();
+			System.out.println("keyCode : " + keyCode);
+			
+			// q를 입력하면 종료되도록 만들기
+			if(keyCode==113) {
+				break; // keyCode가 113과 동일할 경우 실행 종료
+			}
+		}
+
+	
+		System.out.println("종료");
+}
+}
