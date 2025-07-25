d26e2e43c1200813b388e7991081973dfb9e250f
'Hwanghyewon' <'asdd1103@naver.com'>
2025-07-25 17:24:45 +0900

    ㅇ

--- chap07/src/homework01/Member.java
+++ chap07/src/homework01/Member.java
@@ -1,5 +1,21 @@
 package homework01;
 
 public class Member {
-
+	public String memberld;
+	public String memberPwd;
+	public String memberName;
+	public int age;
+	public char gender;
+	public String phone;
+	public String email;
+	
+	public Member (String memberId, String memberPwd, String memberName, int age, char gender, String phone, String email) {
+		this.memberld = memberId;
+		this.memberPwd = memberPwd;
+		this.memberName = memberName;
+		this.age = age;
+		this.gender = gender;
+		this.phone = phone;
+		this.email = email;
+	}
 }
--- chap07/src/homework01/Run.java
+++ chap07/src/homework01/Run.java
@@ -1,5 +1,9 @@
 package homework01;
 
 public class Run {
-
+	public void main(String[] args) {
+		Run run = new Run();
+		
+		
+	}
 }
